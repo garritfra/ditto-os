1a574feaed607f7771fec612d0c0dee0c7562da1
frankeg <garrit.franke@ckc.de>
2020-06-15 21:43:50 +0200

    feat: Enter 32bit gdt

--- print.asm
+++ print.asm
@@ -1,37 +1,27 @@
-print:
+
+[bits 32] ; using 32-bit protected mode
+
+; this is how constants are defined
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f ; the color byte for each character
+
+print_string_pm:
     pusha
+    mov edx, VIDEO_MEMORY
 
-; keep this in mind:
-; while (string[i] != 0) { print string[i]; i++ }
+print_string_pm_loop:
+    mov al, [ebx] ; [ebx] is the address of our character
+    mov ah, WHITE_ON_BLACK
 
-; the comparison for string end (null byte)
-start:
-    mov al, [bx] ; 'bx' is the base address for the string
-    cmp al, 0 
-    je done
+    cmp al, 0 ; check if end of string
+    je print_string_pm_done
 
-    ; the part where we print with the BIOS help
-    mov ah, 0x0e
-    int 0x10 ; 'al' already contains the char
+    mov [edx], ax ; store character + attribute in video memory
+    add ebx, 1 ; next char
+    add edx, 2 ; next video memory position
 
-    ; increment pointer and do next loop
-    add bx, 1
-    jmp start
+    jmp print_string_pm_loop
 
-done:
-    popa
-    ret
-
-
-
-print_nl:
-    pusha
-    
-    mov ah, 0x0e
-    mov al, 0x0a ; newline char
-    int 0x10
-    mov al, 0x0d ; carriage return
-    int 0x10
-    
+print_string_pm_done:
     popa
     ret
